5d8b77156c4f25e058f252c5e3551479b935e720
Tlangelani Mboweni <mbowenit@gmail.com>
2017-06-02 16:33:57 +0200

    add upload section

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,30 @@ var dbRef = firebase.database().ref().child('appName');
 dbRef.on('value', function(snap) {
     appName.innerText = snap.val()
 });
+
+/**
+ * Upload Section
+ */
+var progressBar = document.getElementById('progress-bar');
+var btnUpload = document.getElementById('btn-upload');
+// listen to file selection
+btnUpload.addEventListener('change', function(evt) {
+    // get file
+    var file = evt.target.files[0];
+
+    // create a storage ref
+    var storageRef = firebase.storage().ref('uploads/' + file.name);
+
+    // upload file
+    var task = storageRef.put(file);
+
+    // update progress bar
+    task.on('state_changed', function progress(snapshot) {
+        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        progressBar.value = percentage;
+    }, function error(error) {
+
+    }, function complete() {
+
+    });
+});
