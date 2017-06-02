bfe3d1da9a9987026d1ccabf1bd07db97e440a97
Tlangelani Mboweni <mbowenit@gmail.com>
2017-06-02 15:52:38 +0200

    sync appName variable

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
 // init firebase
 firebase.initializeApp(config);
 
+var appName = document.getElementById('app-name');
+var dbRef = firebase.database().ref().child('appName');
+dbRef.on('value', function(snap) {
+    appName.innerText = snap.val()
+});
